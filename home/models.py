f3f84c52b9f57f24ff9156a99652226fdfa33875
Ebrahimakbari <y560mia3@gmail.com>
2025-03-04 18:54:31 +0330

    feat: add product and category model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(value=self.name)
+        return super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='products/%Y/%m/%d/')
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(value=self.name)
+        return super().save(*args, **kwargs)
