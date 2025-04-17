6a052c68537a688184f51ac40a43efd0c08ecfd2
Ebrahimakbari <y560mia3@gmail.com>
2025-04-17 14:42:29 +0330

    feat: add session base cart

--- orders/cart.py
+++ orders/cart.py
@@ -5,6 +5,22 @@ class Cart:
         self.session = request.session
         cart = self.session.get(CART_SESSION_ID)
         if not cart:
-            self.session[CART_SESSION_ID] = {}
+            cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
-        
+    
+    def __iter__(self):
+        for item in self.cart.values():
+            yield item
+    
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        product_price = str(product.price)
+        if product_id not in self.cart.keys():
+            self.cart[product_id] = {'quantity':0, 'price':product_price, 'total_price':0}
+        self.cart[product_id]['quantity'] += quantity
+        self.cart[product_id]['total_price'] = str(self.cart[product_id]['quantity'] * int(product_price))
+        self.cart[product_id]['product'] = product.name
+        self.cart[product_id]['slug'] = product.slug
+    
+    def save(self):
+        self.session.modified = True
--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views import View
-
+from django.contrib import messages
+from home.models import Product
+from .forms import AddToCartForm
+from .cart import Cart
 
 
 
 class CartView(View):
     def get(self, request):
-        return render(request, 'orders/cart.html', context={})
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', context={'cart':cart})
 
 
 
 class CartAddView(View):
     def post(self, request, product_id):
+        c_form = AddToCartForm(request.POST)
+        product = Product.objects.filter(pk=product_id)
+        if not product.exists():
+            messages.error(request, 'invalid product!!')
+            return redirect('home:home')
+        if c_form.is_valid():
+            cart = Cart(request)
+            product = product.first()
+            cart.add(product, c_form.cleaned_data['quantity'])
+            cart.save()
+            messages.success(request, f'product {product.name} added to cart')
+            return redirect('orders:cart')
+        messages.error(request, 'invalid product quantity!')
         return redirect('home:home')
